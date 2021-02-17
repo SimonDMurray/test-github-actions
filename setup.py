bed5c5a99a38df1a90d3fa9e2e6c0c3931248f5e
Simon Murray <simondonaldmurray@gmail.com>
2021-02-17 16:37:03 +0000

    changed setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Simon", 
-    version="0.0.4",
+    version="0.0.5",
     description="Test GitHub Actions",
     url="https://github.com/pittachalk/sc3s",
     author="Simon",
