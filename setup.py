ce127568ec3d15f682ce3c0a7b0177b6ad03f6d5
Simon Murray <simondonaldmurray@gmail.com>
2021-02-17 15:16:40 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Simon", 
+    version="0.0.4",
+    description="Test GitHub Actions",
+    url="https://github.com/pittachalk/sc3s",
+    author="Simon",
+    author_email="sm42@sanger.ac.uk",
+    classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: BSD License",
+       "Operating System :: OS Independent"
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",    
+    install_requires=[
+       "numpy>=1.19.1", 
+       "pandas>=1.1.3", 
+       "scikit-learn>=0.23.2",
+       "scanpy>=1.6.0" 
+  ]
+)
