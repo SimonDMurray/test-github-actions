587358bf2ce5ce43347a37547d5349663aa2c96d
Simon Murray <simondonaldmurray@gmail.com>
2021-02-18 14:18:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Simon", 
-    version="0.0.10",
+    version="0.0.23",
     description="Test GitHub Actions",
     url="https://github.com/SimonDMurray/test-github-actions/",
     author="Simon",
