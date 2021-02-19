8af735f6d666543bf1c907836011db8512027bfc
Simon <simondonaldmurray@gmail.com>
2021-02-19 14:09:44 +0000

    tidied up repo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Simon", 
-    version="0.0.38",
+    version="0.0.39",
     description="Test GitHub Actions",
     url="https://github.com/SimonDMurray/test-github-actions/",
     author="Simon",
