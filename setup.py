df601f06f335f79e1af8b856e6ad80fb7bbefa63
Simon Murray <simondonaldmurray@gmail.com>
2021-02-19 12:14:36 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Simon", 
-    version="0.0.27",
+    version="0.0.28",
     description="Test GitHub Actions",
     url="https://github.com/SimonDMurray/test-github-actions/",
     author="Simon",
@@ -13,10 +13,20 @@ setup(
     ],
     packages=find_packages(),
     python_requires=">=3.6",    
-    install_requires=[
-       "numpy>=1.19.1", 
-       "pandas>=1.1.3", 
+    install_requires=[ 
+       "numpy>=1.19.2,<2",
+       "pandas>=1.2.2",
+       "python>=3.6",
        "scikit-learn>=0.23.2",
-       "scanpy>=1.6.0" 
+       "scanpy>=1.7.0",
+       "six>=1.15.0",
+       "setuptools>=52.0.0",
+       "joblib>=1.0.0",
+       "mkl-service>=2.3.0",
+       "h5py >=3.1.0",
+       "seaborn",
+       "scipy",
+       "python-dateutil >=2.8.1",
+       "openssl >=1.0.1",
   ]
 )
